d2a409f623d2045271ab0e1f4eac4edb6e72b704
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2026-02-17 12:53:14 +0000

    Fix task 0: Use AsyncGenerator type hint for correct annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,16 +4,16 @@ This module defines a coroutine called async_generator that yields random number
 """
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Loops 10 times, waits 1 second each time, then yields a random number
     between 0 and 10.
 
     Returns:
-        Generator[float, None, None]: An async generator yielding floats.
+        AsyncGenerator[float, None]: An async generator yielding floats.
     """
     for _ in range(10):
         await asyncio.sleep(1)
