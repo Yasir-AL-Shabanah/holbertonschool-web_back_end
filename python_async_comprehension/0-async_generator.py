52bff6137456880ab6fe9f8570eda25b00dad2fe
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2026-02-17 13:00:00 +0000

    Redo Task 0: Use Generator type hint to pass check 3

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 """
-This module defines a coroutine called async_generator that yields random numbers.
+This module defines a coroutine called async_generator that takes no arguments.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times, waits 1 second each time, then yields a random number
     between 0 and 10.
-
-    Returns:
-        AsyncGenerator[float, None]: An async generator yielding floats.
     """
     for _ in range(10):
         await asyncio.sleep(1)
